30de90d26472e16f3030492f093df61690737096
j1mmyson <bwson98@gmail.com>
2022-05-11 15:40:18 +0900

    test code for accounts

---
+++ util/random.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return rand.Int63n(max-min+1) + min
+}
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(b)
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 10000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"KRW", "USD", "EUR"}
+
+	return currencies[rand.Intn(len(currencies))]
+}
