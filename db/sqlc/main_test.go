ac0b7c85e84cbdaab322dea0ef5ae44436f550f1
j1mmyson <bwson98@gmail.com>
2022-09-10 17:04:06 +0900

    Hide config information

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"simple_bank/util"
 	"testing"
 
 	_ "github.com/lib/pq"
@@ -18,8 +19,13 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
 	}
