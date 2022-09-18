3fb457f77313ecd7a2dca1c689eb6c760f5c052f
j1mmyson <bwson98@gmail.com>
2022-09-18 16:57:18 +0900

    불필요한 정보 삭제

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,11 +10,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
